3f6070f4dc6f189b19802c0646c94566f630935c
Bushy <bushy.netshidaulu>
2019-08-18 21:41:09 +0200

    JUnit-5

---
+++ src/main/java/MathsUtils.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Bushy-Netshidaulu
+ * on 8/18/2019.
+ */
+public class MathsUtils {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int subract(int a, int b){
+        return a - b;
+    }
+    public int multiply(int a, int b){
+        return a * b;
+    }
+    public int divide(int a, int b){
+        return a / b ;
+    }
+}
